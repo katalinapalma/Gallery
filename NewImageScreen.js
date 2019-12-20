f0130f56ae9972f4ea38f0135b2c5ee6ff4e1c4e
Ante Hellgren <antehellgren@gmail.com>
2019-12-20 12:54:42 +0100

    file input

--- NewImageScreen.js
+++ NewImageScreen.js
@@ -13,31 +13,47 @@ class NewImageScreen extends Screen{
     this.inputSubmit = document.createElement('input');
     this.inputName = document.createElement('input');
     this.textArea = document.createElement('textarea');
+    this.fileInput = document.createElement('input');
+    this.fileDisplayArea = document.createElement('pre');
 
+    //Set ID for inputs
     this.urlInput.id = 'urlID';
     this.inputName.id = 'imageNameID';
     this.textArea.id = 'description';
     this.inputSubmit.id = 'submitButton';
+    this.fileInput.id = 'file-input';
+    this.fileDisplayArea = 'file-display';
     
+    // URL attributes
     this.urlInput.type = 'url';
     this.urlInput.name = 'image';
     this.urlInput.placeholder = 'Type image url';
 
+    // Image name attributes
     this.inputName.type = 'text';
     this.inputName.name = 'imageName';
     this.inputName.placeholder = 'Name';
 
+    //Description attributes
     this.textArea.placeholder = 'Description of image...';
     this.textArea.rows = '4';
     this.textArea.cols = '50';
 
+    //Submit button attributes
     this.inputSubmit.type = 'submit';
     this.inputSubmit.value = 'submit';
+
+    //File inpit attributes
+    this.fileInput.type = 'file';
+    this.fileInput.name = 'inputfile';
+
 
     this.theForm.appendChild(this.urlInput);
     this.theForm.appendChild(this.inputName);
     this.theForm.appendChild(this.textArea);
     this.theForm.appendChild(this.inputSubmit);
+    this.theForm.appendChild(this.fileInput);
+    this.theForm.appendChild(this.fileDisplayArea);
     this.mainWrapper.appendChild(this.theForm);
 
     console.log(this.theForm);
@@ -57,7 +73,28 @@ class NewImageScreen extends Screen{
       globalObjectArray.push(globalImageObj);
 
       this.theForm.reset();
-    })
+    });
+
+    this.fileInput.addEventListener('change', (e) => {
+      let file = fileInput.files[0];
+      let fileType = /image.*/;
+
+      if (file.type.match(fileType)) {
+        let reader = new FileReader();
+
+        reader.onload = (e) =>{
+          this.fileDisplayArea.innerHTML = '';
+          let img = new Image();
+          img.src = reader.result;
+
+          this.fileDisplayArea.appendChild(img);
+        }
+        reader.readAsDataURL(file);
+      }
+      else {
+        this.fileDisplayArea.innerHTML = 'File not supported';
+      }
+    });
     
   }
   
