10d68886bb007ae5d59308e0f5d7f468d9aa2b61
KarwanG94 <43775769+KarwanG94@users.noreply.github.com>
2020-01-07 16:32:04 +0100

    Modal for gallery + object (#49)

--- GalleryScreen.js
+++ GalleryScreen.js
@@ -3,6 +3,7 @@ class GalleryScreen extends Screen{
     let content = "";
     super(content);
     this.GalleryUI();
+    this.initModal();
   }
 
   GalleryUI() {
@@ -36,6 +37,50 @@ class GalleryScreen extends Screen{
     this.galleryBtn3.id = 'gallery-button3';
     this.galleryBtn3.innerText = 'Create Gallery';
 
+  }
+
+  initModal(){
+    this.mainContent = document.getElementById('main-content-wrapper');
+    this.galleryButton = document.getElementById('gallery-button3');
+
+    this.galleryModal = document.createElement('div'); //creates a new div for gallery modal
+    this.galleryModal.className = 'modal'; // classname for the modal
+    this.galleryModal.id = 'galleryModal'; //gives gallery modal div an id 
+    this.mainContent.appendChild(this.galleryModal); //puts the gallery modal div under the wrapper
+
+    //Modal for gallery modal button
+    this.modalContent = document.createElement('div'); //creates div for gallery modal content
+    this.modalContent.className = 'modal-content-gallery'; //gives the gallery modal div a class name
+    this.galleryModal.appendChild(this.modalContent);
+
+    this.galleryForm = document.createElement('form'); //creates gallery modal form 
+    this.galleryName = document.createElement('input'); //input for name
+    this.galleryName.type = 'text'; // type of text
+    this.galleryName.placeholder = 'Gallery name...'; //a placeholder
+    this.createButtonGallery = document.createElement('button'); //gallery modal button
+    this.createButtonGallery.innerText = 'Create'; // text for the modal button
+
+    this.modalContent.appendChild(this.galleryForm);
+    this.modalContent.appendChild(this.galleryName);
+    this.modalContent.appendChild(this.createButtonGallery);
+
+
+    this.galleryButton.addEventListener('click', (e) => { //when user clicks on gallery modal button, the modal opens
+      this.galleryModal.style.display = 'block';
+    })
     
+    this.createButtonGallery.addEventListener('click', (e) => { // when the user creates a new gallery, a new object is created
+
+     let galleryObj = {
+        name: this.galleryName.value,
+      }
+      e.preventDefault();
+    })
+    
+    window.addEventListener('click', (e) => { //user can click anywhere on window to close modal
+      if(e.target === galleryModal) {
+        this.galleryModal.style.display = 'none';
+      }
+    })
   }
 }
