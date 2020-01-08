afb6da17772bf34c4f978cd838d2b6f3ca55e475
Ante Hellgren <antehellgren@gmail.com>
2020-01-08 16:06:13 +0100

    attaching img to gallery

--- ImageScreen.js
+++ ImageScreen.js
@@ -4,6 +4,7 @@ class ImageScreen extends Screen {
     super(content);
     this.displayImage();
     this.imgModal();
+    this.dropdownListener();
   }
 
   displayImage() {
@@ -21,32 +22,67 @@ class ImageScreen extends Screen {
       this.imgText = document.createElement("p");
       this.removeButton = document.createElement('button');
       this.imgElement = document.createElement("img");
+      this.galleryText = document.createElement("p");
+      
 
       //Setting ID
       this.imgName.id = 'imgNameId' + i;
       this.imgText.id = 'imgTextId' + i;
       this.removeButton.id = 'remove-button' + i;
       this.imgCard.id = 'imgCard' + i;
+      this.galleryText.id = "galleryTextId" + i;
+      
 
       //Setting class
       this.imgCard.className = 'card';
       this.removeButton.className = 'removeButtonClass';
+      this.galleryText.className = "galleryTextClass";
 
       //Setting text
       this.imgName.innerText = globalObjectArray[i].name;
       this.imgText.innerText = globalObjectArray[i].description;
       this.removeButton.innerText = globalObjectArray[i].button;
+      this.galleryText.innerText = "Gallery: ";
+      
  
       //Setting image source
       this.imgElement.src = globalObjectArray[i].url;
       globalObjectArray[i].filey();
+
+      //Gallery selection
+      this.gallerySelectionWrapper = document.createElement("div");
+      this.gallerySelectionWrapper.className = "dropdown";
+      this.imgCard.appendChild(this.gallerySelectionWrapper);
+
+      this.attachGalleryButton = document.createElement("button");
+      this.attachGalleryButton.id = "gallery-button" + i;
+      this.attachGalleryButton.className = "dropbtn";
+      this.attachGalleryButton.onclick = this.toggleDropdown;
+      this.attachGalleryButton.innerText = "Attach";
+      this.gallerySelectionWrapper.appendChild(this.attachGalleryButton);
+
+      this.galleryDropdownList = document.createElement("div");
+      this.galleryDropdownList.id = "myDropdown";
+      this.galleryDropdownList.className = "dropdown-content";
+      this.gallerySelectionWrapper.appendChild(this.galleryDropdownList);
+
+      this.galleryOptionOne = document.createElement("a");
+      this.galleryOptionOne.onclick = () => {
+        this.galleryText.innerHTML = "Gallery: " + globalGalleryObj.name;
+      }
+      this.galleryOptionOne.innerText = globalGalleryObj.name;
+      this.galleryDropdownList.appendChild(this.galleryOptionOne);
     
+
+      
       // Append everything
       this.imgWrapper.appendChild(this.imgCard);
       this.imgCard.appendChild(this.imgElement);
       this.imgCard.appendChild(this.imgName);
       this.imgCard.appendChild(this.imgText);
+      this.imgCard.appendChild(this.galleryText);
       this.imgCard.appendChild(this.removeButton);
+      
 
       this.removeButton.addEventListener('click', (e) => {
         let btnWrapper = document.getElementById('image-wrapper');
@@ -58,6 +94,10 @@ class ImageScreen extends Screen {
 
       this.imgModal(this.imgElement)
     }
+  }
+  setGallery() {
+    console.log("OK");
+    this.galleryText.innerHTML = "Gallery: " + globalGalleryObj.name;
   }
 
   imgModal() { 
@@ -84,6 +124,28 @@ class ImageScreen extends Screen {
       }
     })
 
+  }
+
+  /* When the user clicks on the button,
+      toggle between hiding and showing the dropdown content */
+  toggleDropdown() {
+        document.getElementById("myDropdown").classList.toggle("show");
+  }
+      
+  dropdownListener() {
+      // Close the dropdown menu if the user clicks outside of it
+      window.addEventListener("click", (e) => {
+        if (!e.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      })
   }
 }
 
